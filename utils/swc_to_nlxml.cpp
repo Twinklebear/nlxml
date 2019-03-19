afbe6850dfdaf0f76202b81b99d6b9028f7b50c0
Will Usher <will@sci.utah.edu>
2019-03-19 10:29:16 -0600

    SWC to NLXML format
    SWC is really a mess of a format to parse

--- utils/swc_to_nlxml.cpp
+++ utils/swc_to_nlxml.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iterator>
 #include <vector>
 #include <sstream>
 #include <fstream>
@@ -41,100 +42,110 @@ using namespace nlxml;
  * or -1 to indicate an origin (soma). 
  */
 
+struct SWCPoint {
+	int id;
+	int type;
+	float x, y, z;
+	float radius;
+	int parent_id;
+};
+
+SWCPoint read_point(const std::string &l) {
+	std::istringstream iss(l);
+	std::vector<std::string> vals{std::istream_iterator<std::string>(iss),
+		std::istream_iterator<std::string>()};
+
+	SWCPoint p;
+	p.id = std::stoi(vals[0]);
+	p.type = std::stoi(vals[1]);
+	p.x = std::stof(vals[2]);
+	p.y = std::stof(vals[3]);
+	p.z = std::stof(vals[4]);
+	p.radius = std::stof(vals[5]);
+	p.parent_id = std::stoi(vals[6]);
+	return p;
+}
+
 size_t depth = 1;
 
 // Returns the next line to be read, since in the format we
 // don't know if we're done until we actually go past it
 template<typename T>
-std::string import_swc_tree(std::istream &is, T &branch, int prev_point_id) {
+std::string import_swc_tree(std::istream &is, T &branch, std::string line) {
 	std::string indent(depth, '\t');
-	std::string line;
 	depth += 1;
-	while (true) {
-		if (line.empty()) {
-			if (!std::getline(is, line)) {
-				break;
-			}
-		}
+	do {
 		if (line.empty() || line[0] == '#') {
 			continue;
 		}
-		std::istringstream iss(line);
-		std::vector<std::string> vals{std::istream_iterator<std::string>(iss),
-				std::istream_iterator<std::string>()};
 
-		const int id = std::stoi(vals[0]);
-		const int type = std::stoi(vals[1]);
-		const float x = std::stof(vals[2]);
-		const float y = std::stof(vals[3]);
-		const float z = std::stof(vals[4]);
-		const float radius = std::stof(vals[5]);
-		const int parent_id = std::stoi(vals[6]);
-
-		if (parent_id == -1) {
+		SWCPoint p = read_point(line);
+		if (p.parent_id == -1 || p.type == 1) {
+			depth -= 1;
 			return line;
 		}
 
-		std::cout << indent << line << "\n";
-		// While it would be nice if the SWC files always followed the
-		// type convention described above, it looks like Vaa3D doesn't do
-		// this, so we can't rely on this.
-		if (parent_id != prev_point_id) {
-			// If it's one of our branches parse it, otherwise pop back
-			std::cout << indent << "Branching at " << line << "\n";
-			if (parent_id == prev_point_id) {
-				Branch b;
-				b.points.push_back(Point(x, y, z, radius));
-				line = import_swc_tree(is, b, id);
-				branch.branches.push_back(b);
-			} else {
-				break;
-			}
-		} else {
-			std::cout << indent << "regular point " << line << "\n";
-			branch.points.push_back(Point(x, y, z, radius));
+		if (p.type == 6) {
+			// This branch has ended, we're done
+			branch.points.push_back(Point(p.x, p.y, p.z, p.radius));
+			return "";
+		} else if (p.type == 5) {
+			// We've got a child branch starting at the point we read,
+			// so build all the branches we split into
+			branch.points.push_back(Point(p.x, p.y, p.z, p.radius));
+			int branch_point_id = p.id;
 			line.clear();
-			prev_point_id = id;
+			// We'll either get back a line from a child when it encounters a
+			// new branch that it's not the parent of, or we get an empty line
+			// back and keep reading our own branches
+			while (!line.empty() || std::getline(is, line)) {
+				p = read_point(line);
+				// Check if we're actually the parent of this point we read
+				if (p.parent_id != branch_point_id) {
+					return line;
+				}
+
+				Branch b;
+				b.leaf = "Normal";
+				// The child branches won't return anything to us, since they terminate
+				// when they read their end point
+				line = import_swc_tree(is, b, line);
+				branch.branches.push_back(b);
+			}
+			// This branch ends here, since it forked into two or more branches
+			return "";
+		} else {
+			branch.points.push_back(Point(p.x, p.y, p.z, p.radius));
 		}
-	}
-	depth -=1;
+	} while (std::getline(is, line));
 	return line;
 }
 
-NeuronData import_swc(const std::string &is) {
+NeuronData import_swc(const std::string &fname) {
 	NeuronData data;
 
-	std::ifstream fin(is.c_str());
+	std::ifstream fin(fname.c_str());
 	
 	std::string line;
-	std::getline(fin, line);
-	while (true) {
+	while (!line.empty() || std::getline(fin, line)) {
 		if (line.empty() || line[0] == '#') {
-			std::getline(fin, line);
+			line.clear();
 			continue;
 		}
-		std::istringstream iss(line);
-		std::vector<std::string> vals{std::istream_iterator<std::string>(iss),
-				std::istream_iterator<std::string>()};
-
-		const int id = std::stoi(vals[0]);
-		const int type = std::stoi(vals[1]);
-		const float x = std::stof(vals[2]);
-		const float y = std::stof(vals[3]);
-		const float z = std::stof(vals[4]);
-		const float radius = std::stof(vals[5]);
-		const int parent_id = std::stoi(vals[6]);
+		SWCPoint p = read_point(line);
 
 		// Start of a new tree
-		if (id == 1 || parent_id == -1) {
+		if (p.id == 1 || p.parent_id == -1 || p.type == 1) {
 			Tree t;
-			t.color = Color(0, 0, 1);
-			t.points.push_back(Point(x, y, z, radius));
-			line = import_swc_tree(fin, t, id);
-			data.trees.push_back(t);
-			if (line.empty()) {
-				break;
+			t.color = Color(1, 1, 1);
+			t.type = "Axon";
+			t.leaf = "Normal";
+			t.points.push_back(Point(p.x, p.y, p.z, p.radius));
+
+			if (std::getline(fin, line)) {
+				line = import_swc_tree(fin, t, line);
 			}
+			data.trees.push_back(t);
 		} else {
 			std::cout << "Unexpected normal point?? " << line << "\n";
 			break;
@@ -145,7 +156,6 @@ NeuronData import_swc(const std::string &is) {
 }
 
 int main(int argc, char **argv) {
-	throw std::runtime_error("This is not implemented yet");
 	std::string input, output;
 	for (int i = 1; i < argc; ++i) {
 		if (std::strcmp(argv[i], "-o") == 0) {
